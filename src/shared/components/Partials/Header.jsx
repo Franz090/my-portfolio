6eb95c9e2dba4f3b89073e7658861a416202f431
Francis <48459983+Franz090@users.noreply.github.com>
2023-09-01 21:41:11 +0800

    fix mobile responsive

--- src/shared/components/Partials/Header.jsx
+++ src/shared/components/Partials/Header.jsx
@@ -33,8 +33,13 @@ const HeaderLink = ({ to, text, isActive, onClick }) => (
 const Header = () => {
   const location = useLocation();
   const [activeLink, setActiveLink] = useState('');
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const scrollToTop = () => {
     scrollTo(0);
+    setIsMobileMenuOpen(false); // Close the mobile menu
+  };
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
   };
   // Use useEffect to update the activeLink state when the location changes
   useEffect(() => {
@@ -45,12 +50,19 @@ const Header = () => {
   const handleLinkClick = (to) => {
     scrollTo(0);
     setActiveLink(to);
+    setIsMobileMenuOpen(false); // Close the mobile menu
   };
 
   return (
     <div className="Header pb-28">
-      <nav className=' bg-primary-450 text-white p-9 fixed top-0 left-0 right-0 z-10 '>
-        <ul className="flex space-x-10 items-center justify-between">
+      <nav className={`bg-primary-450 pt-4 text-white p-9 fixed top-0 left-0 right-0 z-10 ${isMobileMenuOpen ? 'mobile-menu-open' : ''}`}>
+      {/* Add hamburger icon */}
+      <div className="mobile-menu-icon" onClick={toggleMobileMenu}>
+        <div className={`bar ${isMobileMenuOpen ? 'active' : ''}`}></div>
+        <div className={`bar ${isMobileMenuOpen ? 'active' : ''}`}></div>
+        <div className={`bar ${isMobileMenuOpen ? 'active' : ''}`}></div>
+      </div>
+        <ul className={`flex  items-center justify-between ${isMobileMenuOpen ? 'mobile-menu-open' : ''}`}>
           <div className="flex items-center">
             <li>
               {/* Apply scrollToTop function to the logo link */}
@@ -59,20 +71,16 @@ const Header = () => {
               </Link>
             </li>
           </div>
-          <div className="flex space-x-8 items-center px-28 nav-color">
-          <HeaderLink to="/" text="HOME" isActive={activeLink === '/'} onClick={() => handleLinkClick('/')} />
-          <HeaderLink to="/about" text="ABOUT" isActive={activeLink === '/about'} onClick={() => handleLinkClick('/about')} />
-         <HeaderLink to="/resume" text="RESUME" isActive={activeLink === '/resume'} onClick={() => handleLinkClick('/resume')} />
-         <HeaderLink to="/skills" text="SKILLS" isActive={activeLink === '/skills'} onClick={() => handleLinkClick('/skills')} />
-         <HeaderLink to="/project" text="PROJECT" isActive={activeLink === '/project'} onClick={() => handleLinkClick('/project')} />
-
-            <li>
-            <a href="#contact" onClick={() => scrollTo(document.getElementById("contact").getBoundingClientRect().top + window.scrollY)} className={`link ${activeLink === '#contact' ? 'active-link' : ''}`}>
-  CONTACT
-</a>
-
-              
-            </li>
+          <div className="flex pt-39 pr-44  nav-color">
+              <HeaderLink to="/" text="HOME" isActive={activeLink === '/'} onClick={() => handleLinkClick('/')} />
+              <HeaderLink to="/about" text="ABOUT" isActive={activeLink === '/about'} onClick={() => handleLinkClick('/about')} />
+              <HeaderLink to="/resume" text="RESUME" isActive={activeLink === '/resume'} onClick={() => handleLinkClick('/resume')} />
+              <HeaderLink to="/skills" text="SKILLS" isActive={activeLink === '/skills'} onClick={() => handleLinkClick('/skills')} />
+              <HeaderLink to="/project" text="PROJECT" isActive={activeLink === '/project'} onClick={() => handleLinkClick('/project')} />
+         <li>
+          <a href="#contact" onClick={() => {handleLinkClick('#contact');scrollTo(document.getElementById("contact").getBoundingClientRect().top + window.scrollY); }} className={`link ${activeLink === '#contact' ? 'active-link' : ''}`}>CONTACT</a>
+          </li>
+        
           </div>
         </ul>
       </nav>
