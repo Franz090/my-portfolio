9f1ee07bdca30a1f1ba50af52866d235beae1456
Francis <48459983+Franz090@users.noreply.github.com>
2023-10-12 23:22:49 +0800

    SAVE

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -7,11 +7,11 @@ function HomePage() {
       <div className="col-span-1 flex flex-col justify-center items-center ">
         <img src={myProfile} alt="myProfile" className='my-profile h-100 w-auto relative  xl:right-[-90px] lg:right-[-40px]' />
       </div>
-      <div className="col-span-1 flex flex-col justify-center items-center relative xl:right-[54px] lg:top-[85px]">
-        <div className="text-center sm:text-center lg:pb-56 md:pb-3 lg:mt-12 md:mb-9 sm:mb-6"> 
-        <h1 className="font-semibold font-montserrat xl:text-7xl text-4xl lg:text-left md:text-6xl md:pb-4 sm:pb-2 xl:pb-2 tracking-wider">FRONT END</h1>
+      <div className="col-span-1 flex flex-col justify-center items-center relative xl:right-[54px] lg:top-[85px] ">
+        <div className="text-center sm:text-center lg:pb-44 md:pb-3 lg:mt-12 md:mb-9 sm:mb-6"> 
+        <h1 className="font-semibold font-montserrat xl:text-7xl text-4xl lg:text-left md:text-6xl md:pb-8 sm:pb-2 xl:pb-5 tracking-wider">FRONTEND</h1>
         <h2 className="font-montserrat font-light lg:text-5xl md:text-5xl sm:text-1xl lg:text-left text-2xl xl:tracking-[.45em] md:tracking-[.45em] tracking-[.45em]">DEVELOPER</h2>
-        <p className='font-montserrat font-normal uppercase text-sm text-quaternary lg:text-left md:text-center xl:mr-36 md:mr-4 relative lg:left-1 md:left-4 md:pt-3 pb-6 md:pb-0 lg:text-1xl'>a front-end developer who's passionate about designing websites and excels at simplifying complex problems. My love for creating user-friendly web experiences and my ability to streamline intricate issues make me an ideal candidate for your team.</p>
+        <p className='font-montserrat font-normal uppercase text-sm text-quaternary lg:text-left md:text-center xl:mr-36 md:mr-4 relative lg:left-1 md:left-4 md:pt-3 pb-6 md:pb-0 lg:text-1xl lg:leading-6 lg:pt-6  pt-3'>As a front-end developer with a passion for web design and a knack for simplifying complex problems, I'm eager to join your team and expand my programming skills.</p>
   </div>
 </div>
     </section>
