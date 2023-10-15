74d2e4a419dd6cc004d05af4dd03ceed86d04740
Francis <48459983+Franz090@users.noreply.github.com>
2023-10-15 18:22:43 +0800

    home fix text

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -14,7 +14,7 @@ function HomePage() {
         <div className="text-center sm:text-center lg:pb-44 md:pb-3 lg:mt-12 md:mb-9 sm:mb-6"> 
         <h1 className="font-montserrat font-semibold xl:text-7xl  text-4xl lg:text-left md:text-6xl lg:text-7xl md:pb-4 sm:pb-2 lg:pb-5 xl:tracking-wider tracking-wider lg:tracking-wide ">FRONTEND</h1>
         <h2 className="font-montserrat font-light lg:text-5xl md:text-4xl sm:text-1xl lg:text-left text-2xl xl:tracking-[.45em] md:tracking-[.45em] tracking-[.45em]">DEVELOPER</h2>
-        <p className='font-montserrat font-medium uppercase text-gray-light lg:text-left md:text-center  text-sm lg:mr-52 lg:text-[.85em] md:text-[.87em]  md:mr-4 relative lg:left-1 md:left-4 md:pt-3 pb-6 md:pb-0 lg:text-1xl lg:leading-6 lg:pt-6  pt-3'> As a web-focused front-end developer, I'm eager to join your team and grow my skills in simplifying complex problems and creating great web designs.</p>
+        <p className=' xl:w-4/6 lg:w-5/6 font-montserrat font-medium uppercase text-gray-light lg:text-left md:text-center  text-sm  lg:text-[.85em] md:text-[.87em]  md:mr-4 relative lg:left-1 md:left-4 md:pt-3 pb-6 md:pb-0 lg:text-1xl lg:leading-6 lg:pt-6  pt-3'> As a web-focused front-end developer, I'm eager to join your team and grow my skills in simplifying complex problems and creating great web designs.</p>
   </div>
 </div>
     </section>
--- src/shared/components/Partials/Contact.jsx
+++ src/shared/components/Partials/Contact.jsx
@@ -3,53 +3,19 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-
-    <div id="contact" className='bg-secondary-450 md:px-10 sm:px-10 xl:px-28  '>
-   
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
-   <h6>This is Contact.</h6>
+      <div id="contact" className='md:px-10 sm:px-10 xl:px-28  '>
+        <h6 className='text-center font-montserrat font-semibold	 lg:text-4xl md:text-3xl text-2xl text-gray-light	'>Contact Me</h6>
+        
+       
 
 
-  </div>
 
-  </div>
+
+
+
+
+        </div>
+   </div>
   )
 }
 
