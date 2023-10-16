d5e0e58810ed4ac85e0ffe23d7affb50bb649e6f
Francis <48459983+Franz090@users.noreply.github.com>
2023-10-16 12:29:07 +0800

    fix lazy load

--- src/shared/components/Partials/Contact.jsx
+++ src/shared/components/Partials/Contact.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 const Contact = () => {
   return (
-    <div>
-      <div id="contact" className='md:px-10 sm:px-10 xl:px-28  '>
-        <h6 className='text-center font-montserrat font-semibold	 lg:text-4xl md:text-3xl text-2xl text-gray-light	'>Contact Me</h6>
-        
-       
+    <>
+      <div id="contact" className="md:px-10 sm:px-10 xl:px-28 "> 
+        <h6 className="text-center font-montserrat font-semibold lg:text-3xl md:text-3xl text-2xl text-gray-light">Contact Me</h6>
 
+        <div className="grid lg:grid-cols-3 grid-cols-1  gap-4 pt-11 ">
+          <div className='col-span-1'>
+          dddd
 
-
-
-
-
-
+          </div>
+          <div className="lg:col-span-2 col-span-1">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur harum magnam vitae facere soluta sapiente eius voluptatem? Suscipit tempora amet ipsam earum neque! Quos repellendus laudantium incidunt temporibus eaque nobis non atque ipsam dicta, corrupti odio quasi odit rerum vitae illum qui nihil tenetur iure aliquam? Fugit delectus dicta blanditiis!
+          </div>
         </div>
-   </div>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default Contact
+export default Contact;
