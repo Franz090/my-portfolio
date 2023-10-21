838f4ac4f2bb3a56e6fbd736968d13130753c8ff
Francis <48459983+Franz090@users.noreply.github.com>
2023-10-21 18:23:01 +0800

    fix the contact form

--- src/components/Contact/ContactInfo.jsx
+++ src/components/Contact/ContactInfo.jsx
@@ -1,11 +1,10 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 
+export default function ContactInfo() {
+ const form = useRef();
 
-export default function ContactInfo()  {
-  const form = useRef();
-
-  const sendEmail = (e) => {
+ const sendEmail = (e) => {
     e.preventDefault();
 
     emailjs.sendForm('service_t9yudsu', 'template_br5ubl1', form.current, 'a1HWmBwln_h1j5REX')
@@ -15,29 +14,39 @@ export default function ContactInfo()  {
       }, (error) => {
           console.log(error.text);
       });
-  };
+ };
+
  return (
     <form className="lg:w-full" ref={form} onSubmit={sendEmail}>
       <div className="flex flex-wrap -mx-3 mb-6">
-        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-          <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="First Name" name="first_name" />
+        <div className="w-full md:w-1/2 px-3">
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
+            First Name
+          </label>
+          <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="text" name="first_name" autoComplete="given-name" />
         </div>
         <div className="w-full md:w-1/2 px-3">
-          <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Last Name"
-          name="last_name" />
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-last-name">
+            Last Name
+          </label>
+          <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" name="last_name" autoComplete="family-name" />
         </div>
         <div className="w-full px-3">
-          <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-email" type="email" placeholder="Email Address" name="user_email" />
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-email">
+            Email Address
+          </label>
+          <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-email" type="email" name="user_email" autoComplete="email" />
         </div>
-        <div className="w-full mb-6 px-3">
-          <textarea className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-message" rows="5" placeholder="Write your message here..." name="message"></textarea>
+        <div className="w-full px-3">
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-message">
+            Message
+          </label>
+          <textarea className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-message" rows="5" name="message" autoComplete="message" />
         </div>
-        <div className="flex justify-center w-full px-3">
-        <input className="bg-gray-light  hover:bg-gray text-white font-bold rounded
-         w-full" type="submit" value="Send" />
       </div>
+      <div className="flex justify-center w-full ">
+        <input className="bg-gray-light pt-3 pb-3 hover-bg-gray-dark text-white font-bold rounded w-full" type="submit" value="Send" />
       </div>
     </form>
  )
 }
-
