4a11c41d08df846b1178961beec08e78432e2ed2
Franz090 <francisoblepias7@gmail.com>
2023-11-07 15:36:31 +0800

    previous and next slide are done

--- src/components/Resume/carousel.jsx
+++ src/components/Resume/carousel.jsx
@@ -1,46 +1,63 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 
 export const Carousel = ({ data }) => {
-    const [slide, setSlide] = useState(0);
-  
-    const nextSlide = () => {
-      setSlide(slide === data.length - 1 ? 0 : slide + 1);
-    };
-  
-    const prevSlide = () => {
-      setSlide(slide === 0 ? data.length - 1 : slide - 1);
-    };
-  
-    return (
-      <div className="carousel relative">
-        <div className="relative flex items-center">
-          <img
-            src={data[slide].src}
-            alt={data[slide].alt}
-            className="slide"
-          />
-          <div className="absolute left-3 top-1/2 transform -translate-y-1/2 ">
+  const [slide, setSlide] = useState(0);
+
+  const nextSlide = () => {
+    if (slide < data.length - 1) {
+      setSlide(slide + 1);
+    }
+  };
+
+  const prevSlide = () => {
+    if (slide > 0) {
+      setSlide(slide - 1);
+    }
+  };
+
+  const goToSlide = (index) => {
+    setSlide(index);
+  };
+
+  useEffect(() => {
+    const slideWidth = 800 / data.length;
+    document.getElementById("slide-container").style.transform = `translateX(-${slide * slideWidth}%)`;
+  }, [slide, data]);
+
+  return (
+    <div className="carousel relative">
+      <div className="relative">
+        <div id="slide-container" className="slide-container">
+          {data.map((item, index) => (
+            <img
+              key={index}
+              src={item.src}
+              alt={item.alt}
+              className="slide"
+            />
+          ))}
+        </div>
+        {slide > 0 && ( 
+          <div className="absolute left-3 top-1/2 transform -translate-y-1/2">
             <BsArrowLeftCircleFill onClick={prevSlide} className="arrow" />
           </div>
+        )}
+        {slide < data.length - 1 && ( 
           <div className="absolute right-11 top-1/2 transform -translate-y-1/2">
             <BsArrowRightCircleFill onClick={nextSlide} className="arrow" />
           </div>
-        </div>
-        <div className="indicators absolute bottom-1  flex ">
-            {data.map((_, idx) => {
-                return (
-                <button 
-                key={idx}
-                className={
-                slide === idx ? "indicator" : "indicator indicator-inactive"
-            }
-            onClick={() => setSlide(idx)}>
-            </button>
-    );
-  })}
-</div>
+        )}
       </div>
-    );
-  };
-  
+      <div className="indicators">
+        {data.map((_, idx) => (
+          <button
+            key={idx}
+            className={slide === idx ? "indicator" : "indicator indicator-inactive"}
+            onClick={() => goToSlide(idx)}
+          ></button>
+        ))}
+      </div>
+    </div>
+  );
+};
