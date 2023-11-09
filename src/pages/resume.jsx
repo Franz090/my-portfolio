952ef22ae22630a21a8d2b77ef05011611a7368c
Franz090 <francisoblepias7@gmail.com>
2023-11-09 17:00:47 +0800

    resume okay

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -45,6 +45,10 @@ const ResumePage = () => {
       },
     ]
   };
+  const responsibilities = [
+    "Creating the user interface (UI) design for adding, viewing, and printing data through Excel on the official website. I also handle hosting and deployment using local network hosting.",
+    "We utilized the system to encode and view data for both accounts payable and non-accounts payable",
+  ];
 
   return (
     <div className="md:px-10 sm:px-10 xl:px-36 pb-8 pt-7">
@@ -61,15 +65,17 @@ const ResumePage = () => {
               <p>February 2023 - May 2023 | Front End Developer</p>
               <p>Santa Cruz, Laguna</p>
             </div>
-            <hr className="my-4 border-t-1 border-custom-gray" />
-            
-          
+            <hr className="my-2 border-t-1 border-custom-gray" />
+            <div className="text-custom-gray">
+               <p className="text-md antialiased ">An official website has been developed to facilitate data record evaluation. It includes features for encoding all data, adding, viewing, printing, and creating backups through Excel in case of important data loss, for both accounts payable and non-accounts payable records.</p>
+              <b className="text-md">Responsibilities</b>
+              <ul className="list-disc pl-5 ml-4">
+                {responsibilities.map((item, index) => (
+                <li key={index}> {item} </li>))}
+                </ul>
+            </div>
           </div>
-
-          
-          
           </div>
-        
         </div>
         <div className="col-span-1 md:col-span-1 lg:pt-11">
           <div className="pt-4 relative flex justify-center items-center w-100 h-100 md:w-600 md:h-400 lg:w-4/7">
