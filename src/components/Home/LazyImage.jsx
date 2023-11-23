a3936242a267e20b8cf72e6dfaa1feb6492a2a9b
Franz090 <francisoblepias7@gmail.com>
2023-11-23 16:53:58 +0800

    lazy image animation done

--- src/components/Home/LazyImage.jsx
+++ src/components/Home/LazyImage.jsx
@@ -5,8 +5,10 @@ import useImageStore from '../../store/useImageStore';
 const LazyImage = () => {
   const isLoading = useImageStore((state) => state.isLoading);
   const setIsLoading = useImageStore((state) => state.setIsLoading);
-  const imageLoaded = useImageStore((state) => state.imageLoaded); 
-  const setImageLoaded = useImageStore((state) => state.setImageLoaded); 
+  const imageLoaded = useImageStore((state) => state.imageLoaded);
+  const setImageLoaded = useImageStore((state) => state.setImageLoaded);
+  const isVisible = useImageStore((state) => state.isVisible);
+  const setIsVisible = useImageStore((state) => state.setIsVisible); 
 
   useEffect(() => {
     const image = new Image();
@@ -14,19 +16,28 @@ const LazyImage = () => {
 
     image.onload = () => {
       setIsLoading(false);
-      setImageLoaded(true); // Set the imageLoaded state to true when the image has loaded
+      setImageLoaded(true);
+      setIsVisible(true); // Set isVisible to true when the image has loaded
     };
-  }, [setIsLoading, setImageLoaded]);
+  }, [setIsLoading, setImageLoaded, setIsVisible]);
 
   return (
-    <div className={`my-profile  xl:right-[-90px] lg:right-[-39px] xl:w-4/7 lg:w-[128rem]  relative`}>
-      {isLoading && <div className='pb-2 text-center text-gray-light font-extralight font-roboto'>Loading...</div>}
-      {imageLoaded && (
+    <div className={`my-profile xl:right-[-90px] lg:right-[-39px] xl:w-4/7 lg:w-[128rem] relative`}>
+      {isLoading && (
+        <div className='pb-2 text-center text-gray-light font-extralight font-roboto'>Loading...</div>
+      )}
+      <div
+        className={`image-container ${isVisible ? 'visible' : ''}`}
+        onAnimationEnd={() => {
+          if (!imageLoaded) setIsVisible(false); // Hide the image after the animation if it's not loaded
+        }}
+      >
         <img
           src={myProfileImage}
           alt="myProfile"
+          className={`image ${isVisible ? 'visible' : ''}`}
         />
-      )}
+      </div>
     </div>
   );
 };
--- src/store/useImageStore.jsx
+++ src/store/useImageStore.jsx
@@ -3,8 +3,10 @@ import { create } from 'zustand';
 const useImageStore = create((set) => ({
   isLoading: true,
   setIsLoading: (isLoading) => set({ isLoading }),
-  imageLoaded: false, // Add the imageLoaded state
-  setImageLoaded: (imageLoaded) => set({ imageLoaded }), // Add a setter for imageLoaded
+  imageLoaded: false,
+  setImageLoaded: (imageLoaded) => set({ imageLoaded }),
+  isVisible: false, // Adding isVisible to the store
+  setIsVisible: (isVisible) => set({ isVisible }), // Setter for isVisible
 }));
 
 export default useImageStore;
