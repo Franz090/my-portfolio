9d5c91d17e92ee46e119db66b062abee190b3099
Franz090 <francisoblepias7@gmail.com>
2023-11-30 12:36:35 +0800

    root layout

--- src/shared/components/Layouts/RootLayout.jsx
+++ src/shared/components/Layouts/RootLayout.jsx
@@ -7,12 +7,13 @@ import StarrySky from '../Partials/StarrySky';
 
 const RootLayout = () => {
  return (
-    <div className="root-main">
-      <StarrySky className="fixed top-0 left-0 z-[-1]" />
-        <Header />
-        <Outlet />
-        <Contact />
-        <Footer />
+   
+     <div className="root-main">
+        <Header className="z-10" />
+        <Outlet className="z-1" />
+        <Contact className="z-10" />
+        <Footer className="z-10" />
+        <StarrySky className="fixed top-0 left-0 z-[-1]" />
     </div>
  );
 };
