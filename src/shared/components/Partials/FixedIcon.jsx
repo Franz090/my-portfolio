c98c4e230ff79bb11281922d0e55f121ba30b8cc
Franz090 <francisoblepias7@gmail.com>
2023-12-25 21:27:26 +0800

    dsa

--- src/shared/components/Partials/FixedIcon.jsx
+++ src/shared/components/Partials/FixedIcon.jsx
@@ -1,12 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useSpring, animated } from '@react-spring/web';
 import useDarkModeStore from '../../../store/useDarkModeStore';
 import useFixedIconStore from '../../../store/useFixedIconStore';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const FixedIcon = () => {
   const [hoveredItem, setHoveredItem] = useState(null);
   const [hideBorder, setHideBorder] = useState(false);
-  const { isDarkMode } = useDarkModeStore(); // Get dark mode state from your store
+  const isFirstMount = useRef(true); // Use ref to track initial mount
+
+  
+
+  const { isDarkMode } = useDarkModeStore();
+  const facebookUrl = 'https://www.facebook.com/dOBLEangtama';
+  const linkedinUrl = 'https://www.linkedin.com/in/oblepiasfrancisp2000';
+  const githubUrl = 'https://github.com/Franz090';
   const {
     isHoveringBorder1,
     isHoveringBorder2,
@@ -14,24 +23,36 @@ const FixedIcon = () => {
     setIsHoveringBorder1,
     setIsHoveringBorder2,
     setIsHoveringBorder3,
+    icon1,
+    icon2,
+    icon3,
+    setIcon1,
+    setIcon2,
+    setIcon3,
   } = useFixedIconStore();
 
   const handleHover = (item) => {
     setHoveredItem(item);
+    setIcon1(item === 1 ? 'ml-3' : 'ml-7'); 
+    setIcon2(item === 2 ? 'ml-3' : 'ml-7'); 
+    setIcon3(item === 3 ? 'ml-3' : 'ml-7'); 
   };
 
   const handleHoverLeave1 = () => {
     setHoveredItem(null);
+    setIcon1('ml-7'); // Reset position on hover out
     handleBorderHover1(true);
   };
   
   const handleHoverLeave2 = () => {
     setHoveredItem(null);
+    setIcon2('ml-7'); // Reset position on hover out
     handleBorderHover2(true);
   };
   
   const handleHoverLeave3 = () => {
     setHoveredItem(null);
+    setIcon3('ml-7'); // Reset position on hover out
     handleBorderHover3(true);
   };
 
@@ -45,6 +66,18 @@ const FixedIcon = () => {
   const handleBorderHover3 = (isHovering) => {
     setIsHoveringBorder3(isHovering);
   };
+  
+
+  useEffect(() => {
+    // Only trigger animation on subsequent renders (not on initial mount)
+    if (!isFirstMount.current) {
+      setIsHoveringBorder1(true);
+      setIsHoveringBorder2(true);
+      setIsHoveringBorder3(true);
+    } else {
+      isFirstMount.current = false; // Update the flag after initial mount
+    }
+  }, []);
 
   const getHoverSpring = (item) => {
     return {
@@ -106,18 +139,17 @@ const FixedIcon = () => {
 
   return (
     <div className="lg:fixed lg:top-64 lg:left-0">
-      <animated.div
+       <animated.div
         onMouseEnter={() => {
           handleHover(1);
           handleBorderHover1(false);
-          setIsHoveringBorder1(false);// Reset the border hover state when entering
+          setIsHoveringBorder1(false); // Reset the border hover state when entering
         }}
         onMouseLeave={handleHoverLeave1} // Set the border hover state when leaving
         style={{
           ...springProps1,
           border: hideBorder ? 'none' : '',
           color: isDarkMode ? '#181818' : '#fffafa',
-          
         }}
         className="transform -translate-y-1/2 h-14  flex justify-center items-center  rounded-tr-full rounded-br-full  mb-2 cursor-pointer"
       >
@@ -126,57 +158,57 @@ const FixedIcon = () => {
             <animated.div style={{ ...slideGridSpring1 }} className="col-span-2 ">
               FACEBOOK
             </animated.div>
-            <div className="ml-7 ">1</div>
+            <div className={icon1}>
+            <FontAwesomeIcon icon={faFacebookF} className="text-[22px]" />
+              </div> 
           </div>
         </animated.div>
       </animated.div>
       <animated.div
-       onMouseEnter={() => {
-        handleHover(2);
-        handleBorderHover2(false);
-        setIsHoveringBorder2(false); // Reset the border hover state when entering
-      }}
-        onMouseLeave={handleHoverLeave2}
+        onMouseEnter={() => {
+          handleHover(2);
+          handleBorderHover2(false);
+          setIsHoveringBorder2(false); // Reset the border hover state when entering
+        }}
+        onMouseLeave={handleHoverLeave2} // Set the border hover state when leaving
         style={{
           ...springProps2,
           border: hideBorder ? 'none' : '',
           color: isDarkMode ? '#181818' : '#fffafa',
-          
         }}
-        className="transform -translate-y-1/2 h-14   flex justify-center items-center  rounded-tr-full rounded-br-full  mb-2 cursor-pointer"
+        className="transform -translate-y-1/2 h-14  flex justify-center items-center  rounded-tr-full rounded-br-full  mb-2 cursor-pointer"
       >
-         <animated.div style={{ ...slideGridSpring2 }}>
-        <div className="grid grid-cols-3 gap-4">
+        <animated.div style={{ ...slideGridSpring2 }}>
+          <div className="grid grid-cols-3 gap-4">
             <animated.div style={{ ...slideGridSpring2 }} className="col-span-2 ">
               LINKEDIN
             </animated.div>
-            <div className="ml-7">2</div>
+            <div className={icon2}>222</div> {/* Adjust position dynamically */}
           </div>
-          </animated.div>
+        </animated.div>
       </animated.div>
       <animated.div
-       onMouseEnter={() => {
-        handleHover(3);
-        handleBorderHover3(false);
-        setIsHoveringBorder3(false);// Reset the border hover state when entering
-      }}
-        onMouseLeave={handleHoverLeave3}
+        onMouseEnter={() => {
+          handleHover(3);
+          handleBorderHover3(false);
+          setIsHoveringBorder3(false); // Reset the border hover state when entering
+        }}
+        onMouseLeave={handleHoverLeave3} // Set the border hover state when leaving
         style={{
           ...springProps3,
           border: hideBorder ? 'none' : '',
           color: isDarkMode ? '#181818' : '#fffafa',
-          
         }}
-        className="transform -translate-y-1/2 h-14   flex justify-center items-center  rounded-tr-full rounded-br-full  mb-2 cursor-pointer"
+        className="transform -translate-y-1/2 h-14  flex justify-center items-center  rounded-tr-full rounded-br-full  mb-2 cursor-pointer"
       >
-         <animated.div style={{ ...slideGridSpring3 }}>
-        <div className="grid grid-cols-3 gap-4">
+        <animated.div style={{ ...slideGridSpring3 }}>
+          <div className="grid grid-cols-3 gap-4">
             <animated.div style={{ ...slideGridSpring3 }} className="col-span-2 ">
-             GITHUB
+              GITHUB
             </animated.div>
-            <div className="ml-7 ">3</div>
+            <div className={icon3}>333</div> {/* Adjust position dynamically */}
           </div>
-          </animated.div>
+        </animated.div>
       </animated.div>
     </div>
   );
--- src/store/useFixedIconStore.jsx
+++ src/store/useFixedIconStore.jsx
@@ -4,9 +4,15 @@ const useFixedIconStore = create((set) => ({
   isHoveringBorder1: false,
   isHoveringBorder2: false,
   isHoveringBorder3: false,
+  icon1: 'ml-7',
+  icon2: 'ml-7',
+  icon3: 'ml-7',
   setIsHoveringBorder1: (isHovering) => set({ isHoveringBorder1: isHovering }),
   setIsHoveringBorder2: (isHovering) => set({ isHoveringBorder2: isHovering }),
   setIsHoveringBorder3: (isHovering) => set({ isHoveringBorder3: isHovering }),
+  setIcon1: (position) => set({ icon1: position }),
+  setIcon2: (position) => set({ icon2: position }),
+  setIcon3: (position) => set({ icon3: position }),
 }));
 
 export default useFixedIconStore;
