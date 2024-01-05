63f42f64de386a520a5bb459fb7548bcfb74c675
Franz090 <francisoblepias7@gmail.com>
2024-01-06 00:25:29 +0800

    skills okay

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -19,6 +19,10 @@ const AboutPage = () => {
       setImagePosition('top-7');
     }
   };
+  const handleHireMeClick = (e) => {
+    e.preventDefault();
+    window.location.href = 'mailto:francisoblepias7@gmail.com'; 
+  };
 
   useEffect(() => {
     // Call handleResize on initial load
@@ -58,12 +62,12 @@ const AboutPage = () => {
   return (
     <section className="grid grid-cols-1 lg:grid-cols-2 gap-0 lg:flex lg:mt-1  lg:pb-10 pb-2 lg:pt-6 md:pt-3 pt-4 md:px-9  xl:px-36 lg:px-20 space-x-3">
       <div className="col-span-1 lg:flex lg:items-start lg:justify-start xl:w-7/12 flex-col">
-      <h1  className="lg:text-[27px] md:text-[27px] sm:text-[27px] text-[26px]  mb-2 tracking-wide capitalize font-semibold">
+      <animated.h1  style={slideFromBottom}  className="lg:text-[27px] md:text-[27px] sm:text-[27px] text-[26px]  mb-2 tracking-wide capitalize font-semibold">
           About Me
-        </h1>
-        <h2 className="lg:text-2xl md:text-2xl  sm:text-2xl text-2xl font-light tracking-wide mb-4">
+        </animated.h1>
+        <animated.h2 style={slideFromBottom}  className="lg:text-2xl md:text-2xl  sm:text-2xl text-2xl font-light tracking-wide mb-4">
   I'm a <TypewriterText texts={['Front End Developer ', 'UI/UX Designer ', 'Freelancer ']} />
-</h2>
+</animated.h2>
         <animated.p style={slideFromBottom} className="text-custom-gray lg:text-[16px] md:text-[16px] sm:text-[16px] text-[15px] font-light mb-3 tracking-tightest antialiased text-justify">
         My name is Francis, and my path in the realm of web development and design has been quite an extraordinary one. Throughout my journey, I've had the privilege of expanding my skills beyond just front-end development and UI design. I've always had a deep-seated passion for creating captivating and immersive digital experiences, and it has been a humbling and fulfilling experience to be able to pursue this passion in my career.
         </animated.p>
@@ -74,14 +78,23 @@ const AboutPage = () => {
           Here is my CV. It contains detailed information about my education, skills, work experience, and projects.
         </animated.p>
         {/* Add the "View CV" button with oblong border radius */}
-        <animated.button 
-        style={slideFromBottom}
-  ref={buttonRef}
-  className="bg-custom-gray-custom hover:bg-zinc-800 py-2 px-5 rounded-full oblong-button tracking-wider text-md font-normal antialiased text-[17px] lg:pt-2 lg:pb-2 md:pt-2 md:pb-2 sm:pt-2 sm:pb-2 pt-1 pb-2 transition duration-300 ease-in-out transform hover:scale-95  text-white" 
-  onClick={handleCVClick}
->
-  {redirecting ? 'Redirecting...' : 'View CV'}
-</animated.button>
+        <div style={{ display: 'flex', gap: '10px' }}>
+    <animated.button
+      style={slideFromBottom}
+      ref={buttonRef}
+      className="bg-custom-gray-custom hover:bg-zinc-800 py-2 px-5 rounded-full oblong-button tracking-wider text-md font-normal antialiased text-[17px] lg:pt-2 lg:pb-2 md:pt-2 md:pb-2 sm:pt-2 sm:pb-2 pt-1 pb-2 transition duration-300 ease-in-out transform hover:scale-95  text-white"
+      onClick={handleCVClick}
+    >
+      {redirecting ? 'Redirecting...' : 'View CV'}
+    </animated.button>
+    <animated.button
+      style={slideFromBottom}
+      className="bg-blue-custom hover:bg-cyan-800 py-2 px-5 rounded-full oblong-button tracking-wider text-md font-normal antialiased text-[17px] lg:pt-2 lg:pb-2 md:pt-2 md:pb-2 sm:pt-2 sm:pb-2 pt-1 pb-2 transition duration-300 ease-in-out transform hover:scale-95  text-white"
+      onClick={handleHireMeClick}
+    >
+      Hire Me
+    </animated.button>
+  </div>
 
       </div>
       <div className={`col-span-1 lg:flex relative ${imagePosition} pb-7 lg:items-center lg:justify-center xl:w-5/12`}>
--- src/pages/skills.jsx
+++ src/pages/skills.jsx
@@ -1,7 +1,88 @@
-const Skills = () => {
-  return (
-    <div className="md:px-10 sm:px-10 xl:px-28">this is skills page</div>
-  )
-}
+import React from 'react';
+import HTML from '../assets/images/skills/HTML.png';
+import CSS from '../assets/images/skills/CSS.png';
+import JAVASCRIPT from '../assets/images/skills/JAVASCRIPT.png';
+import REACTJS from '../assets/images/skills/REACT-JS.png';
+import MYSQL from '../assets/images/skills/MYSQL.png';
+import AJAX from '../assets/images/skills/AJAX.png';
+import JQUERY from '../assets/images/skills/JQUERY.png';
+import JSON from '../assets/images/skills/JSON-API.png';
+import NODEJS from '../assets/images/skills/NODEJS.png';
+import PHP from '../assets/images/skills/PHP.png';
+import ADOBEXD from '../assets/images/skills/ADOBEXD.png';
+import FIGMA from '../assets/images/skills/FIGMA.png';
+import PHOTOSHOP from '../assets/images/skills/PHOTOSHOP.png';
+import GIT from '../assets/images/skills/GIT.png';
+import VITE from '../assets/images/skills/VITE.png';
+import REDUX from '../assets/images/skills/REDUX.png';
+import TAILWIND from '../assets/images/skills/TAILWIND.png';
+import BOOTSTRAP from '../assets/images/skills/BOOTSTRAP.png';
+import SASS from '../assets/images/skills/SASS.png';
+import THREEJS from '../assets/images/skills/THREEJS.png';
+import useDarkModeStore from '../store/useDarkModeStore';
 
-export default Skills
+
+const Skills = () => {
+  const { isDarkMode } = useDarkModeStore();
+
+  const backgroundColor = isDarkMode ? '#181818' : '#fffafa';
+
+ 
+  const skillsList = [
+    { src: HTML, alt: 'HTML Logo', title: 'HTML' },
+    { src: CSS, alt: 'CSS Logo', title: 'CSS' },
+    { src: JAVASCRIPT, alt: 'JAVASCRIPT Logo', title: 'JAVASCRIPT' },
+    { src: REACTJS, alt: 'REACTJS Logo', title: 'REACT JS' },
+    { src: MYSQL, alt: 'MYSQL Logo', title: 'MYSQL' },
+    { src: AJAX, alt: 'AJAX Logo', title: 'AJAX' },
+    { src: JQUERY, alt: 'JQERY Logo', title: 'JQUERY' },
+    { src: JSON, alt: 'JSON Logo', title: 'JSON' },
+    { src: NODEJS, alt: 'NODEJS Logo', title: 'NODE JS' },
+    { src: PHP, alt: 'PHP Logo', title: 'PHP' },
+    { src: ADOBEXD, alt: 'ADOBEXD Logo', title: 'ADOBE XD' },
+    { src: FIGMA, alt: 'FIGMA Logo', title: 'FIGMA' },
+    { src: PHOTOSHOP, alt: 'PHOTOSHOP Logo', title: 'PHOTOSHOP' },
+    { src: GIT, alt: 'GIT Logo', title: 'GIT' },
+    { src: VITE, alt: 'VITE Logo', title: 'VITE' },
+    { src: REDUX, alt: 'REDUX Logo', title: 'REDUX' },
+    { src: TAILWIND, alt: 'TAILWIND Logo', title: 'TAILWIND' },
+    { src: BOOTSTRAP, alt: 'BOOTSTRAP Logo', title: 'BOOTSTRAP' },
+    { src: SASS, alt: 'SASS Logo', title: 'SASS' },
+    { src: THREEJS, alt: 'THREEJS Logo', title: 'THREE JS' },
+    
+  ];
+
+  return (
+    <div className="md:px-10 sm:px-10 xl:px-36 pb-12 pt-7">
+      <h1 className="lg:text-[27px] md:text-[27px] sm:text-[27px] text-2xl mb-5 tracking-wide capitalize font-semibold">
+        Technical Skills
+      </h1>
+      <p className="mb-7">These are the technologies I've worked with</p>
+      <div className="grid grid-cols-5 gap-4 justify-items-center px-4">
+  {/* Map through the skillsList array */}
+  {skillsList.map((skill, index) => (
+    <div key={index} className="text-center lg:w-44 mb-1" >
+      <div className="border-none  rounded-lg lg:p-10 flex flex-col justify-center items-center h-full" 
+      style={{ backgroundColor,
+               transition: ' background-color 0.5s',
+               boxShadow: 'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px'
+                }}>
+        {/* Render images dynamically */}
+        <img
+          src={skill.src}
+          alt={skill.alt}
+          className="h-16 w-16 object-contain max-w-full lg:mb-3 lg:mt-[-10px] "
+        />
+        {/* Apply whitespace-nowrap to keep text on one line */}
+        <span className='lg:flex justify-center items-center text-center h-8 w-8 '>
+          <p className=" whitespace-nowrap text-center antialiased tracking-wide font-normal ">{skill.title}</p>
+        </span>
+      </div>
+    </div>
+  ))}
+</div>
+    </div>
+  );  
+};
+
+export default Skills;
